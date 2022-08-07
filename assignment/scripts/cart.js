6595cb497803a76197f4a7b051ec5bb463c1021e
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-08-07 11:35:31 -0500

    create global maxItems constant

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,7 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+const maxItems = 5;
 
 function addItem(item){
     basket.push(item);
