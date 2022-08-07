4c0fe8519354d45076153fb2006aad52ad152883
Maggie Whitlock <maggie.whitlock@gmail.com>
2022-08-07 11:51:06 -0500

    create and test isFull function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -17,6 +17,16 @@ function empty() {
     return basket;   //instructions did not say to do a return but I did here
 } // end empty
 
+function isFull()   {
+    if(basket.length < maxItems){
+        return false;
+    }
+    else{
+        return true;
+    }
+} // end isFull
+
+
 function listItems(){
     for(item of basket){
         console.log(item);
@@ -33,3 +43,13 @@ console.log('expecting true:', addItem('strawberries'));
 
 console.log(listItems()); //console shows "undefined" because function's return is not defined
 console.log('expecting []', empty());
+
+
+console.log('testing isFull- expecting false', isFull());
+console.log(addItem('grapes'));
+console.log(addItem('milk'));
+console.log(addItem('apples'));
+console.log('testing isFull- expecting false', isFull());
+console.log(addItem('blueberries'));
+console.log(addItem('kiwi'));
+console.log('testing isFull- expecting true', isFull());
